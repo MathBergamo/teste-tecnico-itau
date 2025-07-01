81d8ad671bc82a7304bbc8e112877541ce1320ba
Matheus Bergamo <matheusbergamo@hotmail.com>
2025-07-01 20:19:19 -0300

    initial commit

---
+++ src/test/java/teste/tecnico/itau/ApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.tecnico.itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
