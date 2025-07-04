442cfbd919b57fbd220bebb12f639c13e50cb35a
Matheus Bergamo <matheusbergamo@hotmail.com>
2025-07-03 22:38:46 -0300

    fix: ajustes pom, sql e ids de tabelas

--- src/main/java/teste/tecnico/itau/entity/Ativo.java
+++ src/main/java/teste/tecnico/itau/entity/Ativo.java
@@ -7,21 +7,22 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Entity
 @AllArgsConstructor
+@Getter
 @Table(name = "ativos")
 public class Ativo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false, length = 10, unique = true)
     private String codigo;
 
     @Column(nullable = false, length = 100)
     private String nome;
-
 
 }
